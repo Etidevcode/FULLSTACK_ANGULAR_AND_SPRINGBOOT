3f54ce389309e957b6a26732ea8d14e78e7c00d0
Etidevcode <etixicode@gmail.com>
2024-05-21 16:32:28 +0200

    FULLSTACK ANGULAR AND JAVA SPRING BOOT

--- ecommerce_project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce_project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,27 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
 
+  currentCategroyId: number = 1; //number = 1
+
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
+
   listProducts() {
-    this.productService.getProducList().subscribe(
+
+    // check if "id" parameter is available
+    const hasCategorId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategorId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategroyId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategroyId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProducList(this.currentCategroyId).subscribe(
       data => {
         this.products = data;
       }
